8c899327f3eca76ea16743003715d6ecfab1c345
Thierry Homem de Sá <105328602+ThierryHS@users.noreply.github.com>
2022-11-02 16:59:48 -0300

    Conclusão da atividade usando 'FOR'

--- UC8_LogicaDaProgramacao/ER2_SistemaDeContagem.js
+++ UC8_LogicaDaProgramacao/ER2_SistemaDeContagem.js
@@ -1,6 +1,6 @@
-//ATIVIDADE DO ENCONTRO REMOTO 2.//
+// ATIVIDADE DO ENCONTRO REMOTO 2. //
 
-//CODIFICAÇÃO DE UM SISTEMA DE CONTAGEM E CLASSIFICAÇÃO.//
+// CODIFICAÇÃO DE UM SISTEMA DE CONTAGEM E CLASSIFICAÇÃO. //
 
 /*
 REGRAS PARA O EXERCÍCIO:
@@ -10,3 +10,22 @@ REGRAS PARA O EXERCÍCIO:
 4) Se o número for zero, escreva "zero" e o número correspondente.
 */
 
+let NumeroDeAlunos = 10
+// Contador++ é a mesma coisa que Contador = Contador +1. //
+for (let Contador = 0; Contador <= NumeroDeAlunos; Contador++){
+    console.log(Contador)
+
+    if (Contador == 0){
+        console.log("O número atual é ZERO.")
+    }
+    
+    // OPÇÃO 01 - CONCATENAR.. //
+    else if (Contador % 2 == 0){
+        console.log("O número " + Contador + " é PAR.")
+    }
+    
+    // OPÇÃO 02 - INTERPOLAR. //
+    else{
+        console.log(`O número ${Contador} é ÍMPAR.`)
+    }
+}
