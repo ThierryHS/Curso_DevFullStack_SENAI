868d850df8586bfc0e62315c41ed06e71431fedc
Thierry Homem de Sá <105328602+ThierryHS@users.noreply.github.com>
2022-11-02 16:06:36 -0300

    Definições das regras do exercício

---
+++ UC8_LogicaDaProgramacao/ER2_SistemaDeContagem.js
@@ -0,0 +1,12 @@
+//ATIVIDADE DO ENCONTRO REMOTO 2.//
+
+//CODIFICAÇÃO DE UM SISTEMA DE CONTAGEM E CLASSIFICAÇÃO.//
+
+/*
+REGRAS PARA O EXERCÍCIO:
+1) Percorrer de zero ao número de alunos;
+2) Se o número for par, escreva "par" e o número correspondente;
+3) Se o número for ímpar, escreva "ímpar" e o número correspondente;
+4) Se o número for zero, escreva "zero" e o número correspondente.
+*/
+
